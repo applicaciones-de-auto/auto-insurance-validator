d530f207310f25519bd6b18b9aae9b82330c9576
xurpas7 <michael_cuison07@yahoo.com>
2024-09-26 10:45:20 +0800

    Initial commit

---
+++ src/main/java/org/guanzon/auto/validator/insurance/ValidatorFactory.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.guanzon.auto.validator.insurance;
+
+/**
+ *
+ * @author Arsiela
+ */
+public class ValidatorFactory {
+    public enum TYPE{
+        Policy_Proposal
+    }
+    
+    public static ValidatorInterface make(ValidatorFactory.TYPE foType, Object foValue){
+        switch (foType) {
+            case Policy_Proposal:
+            default:
+                return null;
+        }
+    }
+    
+}
---
+++ src/main/java/org/guanzon/auto/validator/insurance/ValidatorInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.guanzon.auto.validator.insurance;
+
+import org.guanzon.appdriver.base.GRider;
+
+/**
+ *
+ * @author Arsiela
+ */
+public interface ValidatorInterface {
+    public void setGRider(GRider foValue);
+    public boolean isEntryOkay();
+    public String getMessage();
+}
