15ecc4de54667d7b8862b99a3bbb3516f6c175f1
r-sie <32829606+Arsiela@users.noreply.github.com>
2024-09-26 11:39:43 +0800

    09252024-Added functionality for insurance policy proposal
    09252024-Added functionality for insurance policy proposal

--- src/main/java/org/guanzon/auto/validator/insurance/ValidatorFactory.java
+++ src/main/java/org/guanzon/auto/validator/insurance/ValidatorFactory.java
@@ -17,6 +17,7 @@ public class ValidatorFactory {
     public static ValidatorInterface make(ValidatorFactory.TYPE foType, Object foValue){
         switch (foType) {
             case Policy_Proposal:
+                return new Validator_Insurance_Policy_Proposal(foValue);
             default:
                 return null;
         }
--- src/main/java/org/guanzon/auto/validator/insurance/Validator_Insurance_Policy_Proposal.java
+++ src/main/java/org/guanzon/auto/validator/insurance/Validator_Insurance_Policy_Proposal.java
@@ -5,10 +5,77 @@
  */
 package org.guanzon.auto.validator.insurance;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import org.guanzon.appdriver.base.GRider;
+import org.guanzon.auto.model.insurance.Model_Insurance_Policy_Proposal;
+
 /**
  *
  * @author Arsiela
  */
-public class Validator_Insurance_Policy_Proposal {
+public class Validator_Insurance_Policy_Proposal implements ValidatorInterface {
+    GRider poGRider;
+    String psMessage;
+    
+    Model_Insurance_Policy_Proposal poEntity;
+
+    Validator_Insurance_Policy_Proposal(Object foValue){
+        poEntity = (Model_Insurance_Policy_Proposal) foValue;
+    }
+
+    @Override
+    public void setGRider(GRider foValue) {
+        poGRider = foValue;
+    }
+
+    @Override
+    public boolean isEntryOkay() {
+        if(poEntity.getTransNo()== null) {
+            psMessage = "Transaction No is not set.";
+            return false;
+        } else {
+            if (poEntity.getTransNo().isEmpty()){
+                psMessage = "Transaction No is not set.";
+                return false;
+            }
+        }
+//        try {
+//        } catch (SQLException ex) {
+//            Logger.getLogger(Validator_Insurance_Policy_Proposal.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        
+        return true;
+    }
+
+    
+    @Override
+    public String getMessage() {
+        return psMessage;
+    }
+    
+    private static String xsDateShort(Date fdValue) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String date = sdf.format(fdValue);
+        return date;
+    }
+
+    private static String xsDateShort(String fsValue) throws org.json.simple.parser.ParseException, java.text.ParseException {
+        SimpleDateFormat fromUser = new SimpleDateFormat("MMMM dd, yyyy");
+        SimpleDateFormat myFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String lsResult = "";
+        lsResult = myFormat.format(fromUser.parse(fsValue));
+        return lsResult;
+    }
+    
+    /*Convert Date to String*/
+    private LocalDate strToDate(String val) {
+        DateTimeFormatter date_formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate localDate = LocalDate.parse(val, date_formatter);
+        return localDate;
+    }
+    
     
 }
