a977215afbafe504f830bac9886ed5b8c76ba67b
r-sie <32829606+Arsiela@users.noreply.github.com>
2024-10-09 17:12:22 +0800

    10092024 - Updated policy validation
    10092024 - Updated policy validation

--- src/main/java/org/guanzon/auto/validator/insurance/Validator_Insurance_Policy.java
+++ src/main/java/org/guanzon/auto/validator/insurance/Validator_Insurance_Policy.java
@@ -6,10 +6,17 @@
 package org.guanzon.auto.validator.insurance;
 
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.guanzon.appdriver.base.GRider;
+import org.guanzon.appdriver.base.MiscUtil;
+import org.guanzon.appdriver.base.SQLUtil;
+import org.guanzon.appdriver.constant.TransactionStatus;
 import org.guanzon.auto.model.insurance.Model_Insurance_Policy;
 
 /**
@@ -51,10 +58,34 @@ public class Validator_Insurance_Policy implements ValidatorInterface {
                 return false;
             }
         }
-//        try {
-//        } catch (SQLException ex) {
-//            Logger.getLogger(Validator_Insurance_Policy_Proposal.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        try {
+            //Do not allow multiple application for insurance application
+            String lsID = "";
+            String lsDesc = "";
+            String lsSQL = poEntity.makeSelectSQL();
+            lsSQL = MiscUtil.addCondition(lsSQL, " cTranStat <> " + SQLUtil.toSQL(TransactionStatus.STATE_CANCELLED) 
+                                                    + " AND sTransNox <> " + SQLUtil.toSQL(poEntity.getTransNo()) 
+                                                    + " AND sReferNox = " + SQLUtil.toSQL(poEntity.getReferNo())   
+                                                    );
+            System.out.println("EXISTING POLICY CHECK: " + lsSQL);
+            ResultSet loRS = poGRider.executeQuery(lsSQL);
+            if (MiscUtil.RecordCount(loRS) > 0){
+                while(loRS.next()){
+                    lsID = loRS.getString("sTransNox");
+                    lsDesc = xsDateShort(loRS.getDate("dTransact"));
+                }
+
+                MiscUtil.close(loRS);
+
+                psMessage = "Found an existing policy for policy application."
+                            + "\n\n<Policy No:" + lsID + ">"
+                            + "\n<Policy Date:" + lsDesc + ">"
+                            + "\n\nSave aborted.";
+                return false;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Validator_Insurance_Policy.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         return true;
     }
